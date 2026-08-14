ad075d84f2ae3c4a6a4bf0a41c697edbe934383a
Nicola Bertoli <nicolabertoli92@gmail.com@>
2026-08-14 13:07:45 +0200

    updated test
    modified for testingh

--- src/worker.js
+++ src/worker.js
@@ -207,6 +207,7 @@ export default {
           NTFY_URL: Boolean(env.NTFY_URL),
           NTFY_TOKEN: Boolean(env.NTFY_TOKEN),
           BOOKING_KV: Boolean(env.BOOKING_KV),
+          test: env.GOOGLE_PRIVATE_KEY
         });
       }
       if (url.pathname === '/api/availability' && request.method === 'GET') {
